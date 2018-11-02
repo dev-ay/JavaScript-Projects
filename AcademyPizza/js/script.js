4f68d78c0daabbec5eb8e90afd861a6da4f1e11c
dev-ay <44041117+dev-ay@users.noreply.github.com>
2018-11-01 19:02:39 -0700

    Updage Academy Pizza
    -Change "Regular Pizza" to "Large Pizza"
    -Change <title> text
    -Add commenting to HTML, CSS, and JS
    -Update minor code formating

--- AcademyPizza/js/script.js
+++ AcademyPizza/js/script.js
@@ -1,3 +1,4 @@
+//Variables for tracking subtotals and total
 var subSize = 14;
 var subMeat = 0;
 var subCheese = 0;
@@ -6,19 +7,25 @@ var subSauce = 0;
 var subVeggies = 0;
 var sumTotal = 14;
 
+//Calculate overall total and update webpage
 function setTotal() {
     sumTotal = subSize + subMeat + subCheese + subCrust + subSauce + subVeggies;
     document.getElementById('finalTotal').innerHTML = "$" + sumTotal + ".00";
 }
 
+//Calculate subtotal for pizza size, update webpage, and call setTotal()
 function setSize() {
     sizeObject = document.getElementsByName('size');
     selection = "";
+
+    //Find which radio button was selected
     for(var i=0;i<sizeObject.length;i++){
         if (sizeObject[i].checked == true){
             selection = sizeObject[i].value;
         }
     }
+
+    //Update subtotal and webpage based on user selection
     switch(selection) {
         case 'Personal':
             subSize = 6;
@@ -39,16 +46,20 @@ function setSize() {
     }
     document.getElementById('sizeA').innerHTML = selection + " Pizza";
     document.getElementById('sizeB').innerHTML = "$" + subSize + ".00";
+
+    //Call setTotal()
     setTotal();
 }
 
-
+//Calculate subtotal for meat selection, update webpage, and call setTotal()
 function setMeat() {
     meatObject = document.getElementsByName('meat');
-    count = 0;
-    var temp = new Array();
+    count = 0; //counts how many meats selected
+    var temp = new Array(); //array holding text for each selected meat
     var i;
-    var txtMeat;
+    var txtMeat; //holds joined text for all selected meats
+
+    //Find which meat was selected and add it to temp array, increment count
     for(i=0;i<meatObject.length;i++){
         if (meatObject[i].checked == true){
             count++;
@@ -56,8 +67,10 @@ function setMeat() {
         }
     }
 
+    //Convert temp array into single comma-delineated sentence
     txtMeat = temp.join(', ');
 
+    //Calculate and display proper cost
     if(count==0){
         subMeat = 0;
         document.getElementById('meatTotal').innerHTML = "+ $" + subMeat;
@@ -67,23 +80,25 @@ function setMeat() {
         document.getElementById('meatTotal').innerHTML = "+ $" + subMeat;
         document.getElementById('meatA').innerHTML = txtMeat;
     }
-
-
-
-    
     document.getElementById('meatB').innerHTML = "$" + subMeat + ".00";
+
+    //Call setTotal()
     setTotal();
 }
 
-
+//Calculate subtotal for cheese selection, update webpage, and call setTotal()
 function setCheese() {
     cheeseObject = document.getElementsByName('cheese');
-    selection = "";
+    selection = ""; //holds text for selected cheese
+
+    //Find which radio button was selected
     for(var i=0;i<cheeseObject.length;i++){
         if (cheeseObject[i].checked == true){
             selection = cheeseObject[i].value;
         }
     }
+
+    //Update subtotal and webpage based on user selection
     switch(selection) {
         case 'Extra Cheese':
             subCheese = 3;
@@ -97,19 +112,25 @@ function setCheese() {
     }
     document.getElementById('cheeseA').innerHTML = selection;
     document.getElementById('cheeseB').innerHTML = "$" + subCheese + ".00";
+    
+    //Call setTotal()
     setTotal();
 }
 
 
-
+//Calculate subtotal for crust selection, update webpage, and call setTotal()
 function setCrust() {
     crustObject = document.getElementsByName('crust');
-    selection = "";
+    selection = ""; //holds text for selected crust
+
+    //Find which radio button was selected
     for(var i=0;i<crustObject.length;i++){
         if (crustObject[i].checked == true){
             selection = crustObject[i].value;
         }
     }
+
+    //Update subtotal and webpage based on user selection
     switch(selection) {
         case 'Cheese Stuffed':
             subCrust = 3;
@@ -123,26 +144,36 @@ function setCrust() {
     }
     document.getElementById('crustA').innerHTML = selection + " Crust";
     document.getElementById('crustB').innerHTML = "$" + subCrust + ".00";
+
+    //Call setTotal()
     setTotal();
 }
 
+//Find sauce selection and update webpage (cost always $0 and unchanged)
 function setSauce() {
     sauceObject = document.getElementsByName('sauce');
-    selection = "";
+    selection = ""; //holds text for selected crust
+
+    //Find which radio button was selected
     for(var i=0;i<sauceObject.length;i++){
         if (sauceObject[i].checked == true){
             selection = sauceObject[i].value;
         }
     }  
+
+    //Update webpage based on user selection
     document.getElementById('sauceA').innerHTML = selection + " Sauce";
 }
 
+//Calculate subtotal for veggie selection, update webpage, and call setTotal()
 function setVeggies() {
     veggiesObject = document.getElementsByName('veggies');
-    count = 0;
-    var temp = new Array();
+    count = 0; //counts how many veggies selected
+    var temp = new Array(); //array holding text for each selected veggie
     var i;
-    var txtVeggies;
+    var txtVeggies; //holds joined text for all selected veggies
+
+    //Find which veggie was selected and add it to temp array, increment count
     for(i=0;i<veggiesObject.length;i++){
         if (veggiesObject[i].checked == true){
             count++;
@@ -150,8 +181,10 @@ function setVeggies() {
         }
     }
 
+    //Convert temp array into single comma-delineated sentence
     txtVeggies = temp.join(', ');
 
+    //Calculate and display proper cost
     if(count==0){
         subVeggies = 0;
         document.getElementById('veggiesTotal').innerHTML = "+ $" + subVeggies;
@@ -161,15 +194,19 @@ function setVeggies() {
         document.getElementById('veggiesTotal').innerHTML = "+ $" + subVeggies;
         document.getElementById('veggiesA').innerHTML = txtVeggies;
     }
-
-    
     document.getElementById('veggiesB').innerHTML = "$" + subVeggies + ".00";
+
+    //Call setTotal()
     setTotal();
 }
 
+//Call by Reset button from webpage.  Resets form values to defaults
+//Hides order receipt
 function startOver() {
     document.getElementById('mainForm').reset();
     document.getElementById('receipt').style.opacity = 0;
+
+    //Reset subtotals and total to original values
     subSize = 14;
     subMeat = 0;
     subCheese = 0;
@@ -177,6 +214,8 @@ function startOver() {
     subSauce = 0;
     subVeggies = 0;
     sumTotal = 14;
+
+    //Update webpage
     setSize();
     setMeat();
     setCheese();
@@ -186,6 +225,7 @@ function startOver() {
 
 }
 
+//Called by "Place Order" button.  Displays order receipt
 function order() {
     document.getElementById('receipt').style.opacity = 1;
 }
